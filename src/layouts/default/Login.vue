e54d83b7ea358530cd066df2defd46b0fd01b1d9
Caio Bolognese <ti.caio@unieduk.com>
2023-08-07 23:47:54 -0300

    first commit

---
+++ src/layouts/default/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-main>
+    <v-card loading title="Card title" text="..." variant="outlined">
+      <v-card-actions>
+        <v-btn>Click me</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-main>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
