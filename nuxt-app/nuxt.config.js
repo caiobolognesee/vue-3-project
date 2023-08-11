acffde6317fa8026267d62254d84e41933ae7a2b
Caio Bolognese <bolognese8891@hotmail.com>
2023-08-11 16:35:09 -0300

    add nuxt

---
+++ nuxt-app/nuxt.config.js
@@ -0,0 +1,19 @@
+export default {
+  // Configurações do Nuxt.js
+  // ...
+
+  // Configurações de módulos
+  modules: [
+    // Adicione os módulos que você deseja usar
+    // Exemplo: '@nuxt/http'
+  ],
+
+  // Configurações de plugins
+  plugins: [
+    // Adicione os plugins que você deseja usar
+    // Exemplo: '~/plugins/my-plugin.js'
+  ],
+
+  // Configurações de roteamento
+  // ...
+}
