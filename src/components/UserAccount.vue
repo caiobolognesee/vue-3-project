33da9a8df2e192a74b05dcecc039f44a9f96b27f
Caio Bolognese <bolognese8891@hotmail.com>
2023-10-23 13:43:35 -0300

    implementing menu to user account

---
+++ src/components/UserAccount.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-menu v-model="showUser" :close-on-content-click="false" location="end">
+    <v-card min-width="300">
+      <v-list>
+        <v-list-item prepend-avatar="https://cdn.vuetifyjs.com/images/john.jpg" title="John Leider"
+          subtitle="Founder of Vuetify">
+        </v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item>
+          <v-switch v-model="message" color="purple" label="Enable messages" hide-details></v-switch>
+        </v-list-item>
+
+        <v-list-item>
+          <v-switch v-model="hints" color="purple" label="Enable hints" hide-details></v-switch>
+        </v-list-item>
+      </v-list>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn variant="text" @click="showUserMenu">
+          Cancel
+        </v-btn>
+        <v-btn color="primary" variant="text" @click="showUserMenu">
+          Save
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-menu>
+</template>
+
+<script setup>
+import { reactive, computed } from 'vue'
+
+const props = defineProps({
+  showUserMenu: {
+    type: Function,
+    required: false
+  },
+  showData: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const teste = reactive({
+  teste1: props.showData
+})
+
+// a computed ref
+const showUser = computed(() => {
+  console.log('aq', teste.teste1)
+  return teste.teste1
+})
+
+</script>
+
+<style scoped>
+</style>
--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -1,38 +1,54 @@
 <template>
-    <v-layout>
-      <v-app-bar color="grey" title="Caio Bolognese" :elevation="1" rounded>
-        <template v-slot:append>
-          <v-btn icon="mdi-linkedin" target="_blank" href="https://www.linkedin.com/in/caio-bolognese-43a41613b/" />
-          <v-btn icon="mdi-instagram" target="_blank" href="https://instagram.com/caio.bolognese?igshid=MjEwN2lyYWYwYw==" />
-          <v-btn icon="mdi-facebook" target="_blank" href="https://www.facebook.com/profile.php?id=100090405788391" />
-        </template>
-      </v-app-bar>
+  <v-layout>
+    <v-app-bar color="grey" title="Caio Bolognese" :elevation="1" rounded>
+      <template v-slot:append>
+        <v-btn icon="mdi-linkedin" target="_blank" href="https://www.linkedin.com/in/caio-bolognese-43a41613b/" />
+        <v-btn icon="mdi-instagram" target="_blank" href="https://instagram.com/caio.bolognese?igshid=MjEwN2lyYWYwYw==" />
+        <v-btn icon="mdi-facebook" target="_blank" href="https://www.facebook.com/profile.php?id=100090405788391" />
+        <v-btn icon="mdi-dots-vertical" @click="showUserMenu()" />
+        <v-card min-width="300" v-show="account">
+          <UserAccountVue
+            :showData="account"
+            :showUserMenu="showUserMenu"
+          />
+        </v-card>
+      </template>
+    </v-app-bar>
 
-      <v-navigation-drawer expand-on-hover :elevation="5" rail>
-        <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-home" title="Home" to="/home" />
-          <!-- <v-list-item prepend-icon="mdi-widgets" title="Dashboard" to="/dashboard" /> -->
-          <v-list-item prepend-icon="mdi-gas-station" title="Spending Average" to="/spendingAverage" />
-        </v-list>
-        <template v-slot:append>
-              <v-list-item prepend-icon="mdi-logout" title="Sair" to="/login" @click="logout()"/>
-          </template>
-      </v-navigation-drawer>
+    <v-navigation-drawer expand-on-hover :elevation="5" rail>
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-home" title="Home" to="/home" />
+        <!-- <v-list-item prepend-icon="mdi-widgets" title="Dashboard" to="/dashboard" /> -->
+        <v-list-item prepend-icon="mdi-gas-station" title="Spending Average" to="/spendingAverage" />
+      </v-list>
+      <template v-slot:append>
+        <v-list-item prepend-icon="mdi-logout" title="Sair" to="/login" @click="logout()" />
+      </template>
+    </v-navigation-drawer>
 
-      <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
-        <router-view></router-view>
-      </v-main>
-    </v-layout>
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+      <router-view></router-view>
+    </v-main>
+  </v-layout>
 </template>
 
 <script setup>
+import UserAccountVue from "@/components/UserAccount.vue"
+
+let account = false
+
+function showUserMenu() {
+  console.log('1', account)
+  account = !account
+}
 </script>
 
 <style scoped>
-.logout{
+.logout {
   align-content: center;
 }
+
 .background-navbar {
-  background-color:darkgray
+  background-color: darkgray
 }
 </style>
